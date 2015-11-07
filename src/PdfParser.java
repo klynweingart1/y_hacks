1682c64fe844a74b3b6ea970ef0a7eedadcdfb06
Katelyn <klynweingart@gmail.com>
2015-11-06 23:17:12 -0500

    first commit

---
+++ src/PdfParser.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.StatusLine;
+import org.apache.http.client.*;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+public class PdfParser {
+	
+}
